611a7fdc51365e2771c2681689f67983708a7652
Paweł Augustyn <pawelaugustyn15@gmail.com>
2017-03-14 23:29:59 +0100

    Rozwiazanie problemu 1

--- lab_01/Problem_1/logparser.py
+++ lab_01/Problem_1/logparser.py
@@ -1,17 +1,41 @@
 from os.path import isfile
-
+import re
 
 class LogParser:
     @staticmethod
     def parse_log(input_file, string_to_be_found):
         if isfile(input_file):
             with open(input_file) as file:
+                finalarray = []
                 lines = []
                 for line in file:
                     line = line.strip()
                     lines.append(line)
                 matching_lines = [x for x in lines if string_to_be_found in x]
-                return matching_lines
+                matching_lines = "\n".join(matching_lines)
+                matching_lines = re.sub(' +', ' ', matching_lines)
+                matching_lines = re.sub(' clones', ' ', matching_lines)
+                matching_lines = re.sub('PrChecker.Downs... INFO ?\n', '', matching_lines)
+                matching_lines = re.sub('PrChecker.Downs... INFO ?', ' ', matching_lines)
+                matching_lines = re.sub(' +', ' ', matching_lines)
+                matching_lines = re.sub('\n+', '\n', matching_lines)
+                matching_lines = re.sub('\*+.+\*+', '', matching_lines)
+                matching_lines = re.sub(': ', '', matching_lines)
+                matching_lines = re.sub('purity', '', matching_lines)
+                matching_lines = re.sub('from ', '', matching_lines)
+                matching_lines = re.sub('hitEff', '', matching_lines)
+                matching_lines = re.sub(', ', ' ', matching_lines)
+                matching_lines = re.sub(' %', '%', matching_lines)
+                matching_lines = re.sub(' \[', '', matching_lines)
+                matching_lines = re.sub('\]', '', matching_lines)
+                words = matching_lines.split("\n")
+                words = [x for x in words if len(x) > 1]
+                for word in words:
+                    word = word.split(' ')
+                    arraytoinsert = [x for x in word if len(x) > 1]
+                    if len(arraytoinsert) > 0 and re.match('[A-Za-z]{1}.+', arraytoinsert[0]):
+                        finalarray.append(arraytoinsert)
+                return finalarray
         else:
             raise FileNotFoundError("Brak pliku")
 
--- lab_01/Problem_1/problem1.py
+++ lab_01/Problem_1/problem1.py
@@ -1,11 +1,12 @@
 import argparse
-from logparser import LogParser
-from validatefilepath import InputFileValidator
 
+from lab_01.Problem_1.validatefilepath import InputFileValidator
+
+from lab_01.Problem_1.logparser import LogParser
 
 parser = argparse.ArgumentParser(description="Simple LogParser which checks for string in specified file")
-parser.add_argument("file_to_parse")
-parser.add_argument("string_to_be_found")
+parser.add_argument("file_to_parse", help="Name of file where you will look for a string")
+parser.add_argument("string_to_be_found", help="String that you'll be looking for")
 args = parser.parse_args()
 file_to_parse = args.file_to_parse
 string_to_be_found = args.string_to_be_found
@@ -13,14 +14,14 @@ Validator = InputFileValidator(file_to_parse)
 
 if Validator.validate():
     result = ""
+    finalarray = []
     for file in Validator.get_files_list():
         added = LogParser.parse_log(file, string_to_be_found)
         if added:
-            result += "Plik {}:\n".format(file)
-            result += "\n".join(added) + "\n\n"
-    if len(result) > 0:
-        logs = open("logged_text.log", "w")
-        logs.write(result)
-        logs.close()
+            finalarray = finalarray + added
+
+    for i in range(0, len(finalarray)):
+        print(finalarray[i])
+
 else:
     raise FileNotFoundError("Nie znaleziono pliku {}".format(file_to_parse))
